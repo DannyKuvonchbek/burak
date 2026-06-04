921d1f46291b6afbceff925c1b38b8d7baf4b76f
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-06-04 15:51:32 +0900

    feat: O-TASK and P-TASk

--- src/train.ts
+++ src/train.ts
@@ -7,3 +7,31 @@
       css => SNAKE                           button_style
   - Error handling
 */ 
+// O-TASK
+function calculateSumOfNumbers(arr: any[]): number {
+  let total = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      total += arr[i];
+    }
+  }
+
+  return total;
+}
+
+console.log(calculateSumOfNumbers([10, "10", { son: 10 }, true, 35]));
+
+// P-TASK
+function objectToArray(obj: any): any[] {
+  let result: any[] = [];
+
+  for (let key in obj) {
+    result.push([key, obj[key]]);
+  }
+
+  return result;
+}
+
+console.log(objectToArray({ a: 10, b: 20 }));
+
