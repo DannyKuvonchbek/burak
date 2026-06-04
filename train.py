ecc6ad28816a7061a90efc15333d54f78e7504d5
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-06-04 15:47:53 +0900

    feat: TASK O and TASK P

--- train.py
+++ train.py
@@ -1,3 +1,32 @@
+# P-TASK
+def objectToArray(obj):
+    result = []
+
+    for key in obj:
+        result.append([key, obj[key]])
+
+    return result
+
+
+print(objectToArray({"a": 10, "b": 20}))
+
+# O-TASK
+
+
+def calculateSumOfNumbers(arr):
+    total = 0
+
+    for item in arr:
+        if type(item) == int or type(item) == float:
+            total += item
+
+    return total
+
+
+print(calculateSumOfNumbers([10, "10", {"son": 10}, True, 35]))
+
+
+'''
 # N-TASK
 def palindrom_check(str):
     reversed_text = str[::-1]
@@ -7,8 +36,8 @@ def palindrom_check(str):
     else:
         return False
 
-
 print(palindrom_check("dad"))
+'''
 
 # M-TASK
 '''
