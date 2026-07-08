c0282f8105fbc3adaa7ad3b53925ee79988de56a
DannyKuvonchbek <sobirovich9702@gmail.com>
2026-07-08 16:12:41 +0900

    feat: develop getProduct rest Api

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -7,7 +7,7 @@ import {
   ProductInquiry,
   ProductUpdateInput,
 } from "../libs/types/product";
-import { AdminRequest } from "../libs/types/member";
+import { AdminRequest, ExtendedRequest } from "../libs/types/member";
 import { ProductCollection } from "../libs/enums/product.enum";
 
 const productService = new ProductService();
@@ -34,6 +34,21 @@ productController.getProducts = async (req: Request, res: Response) => {
     res.status(HttpCode.OK).json(result);
   } catch (err) {
     console.log("Error, getProducts:", err);
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart);
+  }
+};
+
+productController.getProduct = async (req: ExtendedRequest, res: Response) => {
+  try {
+    console.log("getProduct");
+    const { id } = req.params;
+    const memberId = req.member?._id ?? null,
+      result = await productService.getProduct(memberId, id as string);
+
+    res.status(HttpCode.OK).json(result);
+  } catch (err) {
+    console.log("Error, getProduct:", err);
     if (err instanceof Errors) res.status(err.code).json(err);
     else res.status(Errors.standart.code).json(Errors.standart);
   }
